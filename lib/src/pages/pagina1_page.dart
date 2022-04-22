1024a5723ea4d75db93d6c229db2ff793fa193db
LuisPulenta <luisalbertonu@gmail.com>
2022-04-22 12:53:48 -0300

    Video 195

--- lib/src/pages/pagina1_page.dart
+++ lib/src/pages/pagina1_page.dart
@@ -1,22 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:transicion_app/src/pages/pagina2_page.dart';
 
 class Pagina1Page extends StatelessWidget {
-  const Pagina1Page({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Pagina 1'),
-        centerTitle: true,
+        title: Text('Página'),
       ),
       body: Center(
         child: Text('Pagina 1'),
       ),
       floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.next_plan_outlined),
-        onPressed: () {},
-      ),
+          child: Icon(Icons.access_time),
+          onPressed: () {
+            Navigator.push(context, _crearRuta());
+          }),
     );
+  }
+
+  Route _crearRuta() {
+    return PageRouteBuilder(
+        pageBuilder: (BuildContext context, Animation<double> animation,
+                Animation<double> secondaryAnimation) =>
+            Pagina2Page(),
+        transitionDuration: Duration(seconds: 2),
+        transitionsBuilder: (context, animation, secondaryAnimation, child) {
+          final curvedAnimation =
+              CurvedAnimation(parent: animation, curve: Curves.easeInOut);
+
+          // return SlideTransition(
+          //   position: Tween<Offset>(begin: Offset(0.5, -1.0), end: Offset.zero)
+          //       .animate(curvedAnimation),
+          //   child: child,
+          // );
+
+          // return ScaleTransition(
+          //     child: child,
+          //     scale:
+          //         Tween<double>(begin: 0.0, end: 1.0).animate(curvedAnimation));
+
+          // return RotationTransition(
+          //     child: child,
+          //     turns:
+          //         Tween<double>(begin: 0, end: 1.0).animate(curvedAnimation));
+
+          // return FadeTransition(
+          //     child: child,
+          //     opacity:
+          //         Tween<double>(begin: 0.0, end: 1.0).animate(curvedAnimation));
+
+          // return RotationTransition(
+          //     child: FadeTransition(
+          //         child: child,
+          //         opacity: Tween<double>(begin: 0.0, end: 1.0)
+          //             .animate(curvedAnimation)),
+          //     turns:
+          //         Tween<double>(begin: 0.0, end: 1.0).animate(curvedAnimation));
+
+          return ScaleTransition(
+              child: FadeTransition(
+                  child: child,
+                  opacity: Tween<double>(begin: 0.0, end: 1.0)
+                      .animate(curvedAnimation)),
+              scale:
+                  Tween<double>(begin: 0.0, end: 1.0).animate(curvedAnimation));
+        });
   }
 }
